5ced8a1c154f6928e9dda9a46c123e1f3ff95c71
Evelin R <everosales03.4@gmail.com>
2026-05-21 15:22:54 -0500

    ajustes finales

--- album/views.py
+++ album/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
-from django.views.generic import ListView
-from album.models import Team
+from django.views.generic import ListView, DetailView
+from django.db.models import Prefetch
+from album.models import Team, Player
 
 class TeamListView(ListView):
     model = Team
+    paginate_by = 12
+
+class TeamDetailView(DetailView):
+    model = Team
+    queryset = Team.objects.prefetch_related(
+        Prefetch('get_players', queryset=Player.objects.all())
+    )
+ 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['players'] = self.object.get_players.all()
+        return context
--- mundial2026/urls.py
+++ mundial2026/urls.py
@@ -22,5 +22,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('team/',views.TeamListView.as_view(),name='team-list'),
+    path('team/', views.TeamListView.as_view(), name='team-list'),
+    path('team/<int:pk>/', views.TeamDetailView.as_view(), name='team-detail'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
